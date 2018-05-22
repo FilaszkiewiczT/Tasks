417a0232688f7f94e7a50499ccbbbfdd61da1c36
“FilaszkiewiczT <filaszkiewicz.t@gmail.com>
2018-05-22 20:15:29 +0200

    Zadanie 19.4 v2

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -23,8 +23,9 @@ public class EmailScheduler {
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String taskOrTasks = "task";
-        if(size>1) taskOrTasks = "tasks";
-
+        if (size > 1) {
+            taskOrTasks = "tasks";
+        }
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT, "Currently in database you got: " + size + taskOrTasks, "")
